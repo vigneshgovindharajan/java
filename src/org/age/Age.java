b8de8e70801de1cb35a80ac9bbd24589726ec80a
javaclass <gvigneshmech201@gmail.com>
2022-09-22 16:13:16 +0530

    namejava

---
+++ src/org/age/Age.java
@@ -0,0 +1,15 @@
+package org.age;
+
+public class Age {
+	public static void main(String[]args) {
+		int a=10;
+		if (a>=18){
+			System.out.println("eligible for vote");
+		
+	}
+else{
+		System.out.println("not eligible for vote");
+	}
+
+}
+}
